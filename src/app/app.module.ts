cf7f2b362d90dbfc32072ee7362069625b9767cd
Yosuf <yosuf.haydary@gmail.com>
2018-11-09 23:04:54 +0100

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SkillsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css/dist/js/materialize.min.js'
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
+})
+export class SkillsComponent implements OnInit {
+  hardSkills = [
+    { "name":"Java stack engineering",
+    "stars": ["star","star","star","star","star"] },
+    {"name":"CI/CD",
+    "stars": ["star","star","star","star","star"] },
+    {"name":"DevOps",
+    "stars": ["star","star","star","star_half","star_border"] },
+    {"name":"Full-stack engineering",
+    "stars": ["star","star","star","star","star_border"] },
+    {"name":"Software test automation",
+    "stars": ["star","star","star","star","star_border"] },
+    {"name":"Software architecture",
+      "stars": ["star","star","star","star","star_border"] },
+  ]
+
+  softSkills = [
+    { "name":"Technical team leading",
+    "stars": ["star","star","star","star","star"] },
+    {"name":"Agility",
+    "stars": ["star","star","star","star","star_half"] },
+    {"name":"Motivating/Inspiring teams",
+    "stars": ["star","star","star","star_half","star"] },
+    {"name":"Taking ownership",
+    "stars": ["star","star","star","star","star"] },
+    {"name":"Communication",
+    "stars": ["star","star","star","star","star"] },
+    {"name":"Communication",
+      "stars": ["star","star","star","star_half","star_border"] },
+  ]
+
+  options = {"accordion":false}
+constructor() { }
+
+ngOnInit() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, this.options);
+}
+
+}
