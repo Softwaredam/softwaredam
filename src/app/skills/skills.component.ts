2fc96518aaec3fb7fda7eec987f3fd318a20eb01
Yosuf <yosuf.haydary@gmail.com>
2018-11-11 17:19:05 +0100

    Correct skills and colorise stars

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,41 +8,40 @@ import * as M from 'materialize-css/dist/js/materialize.min.js'
 })
 export class SkillsComponent implements OnInit {
   hardSkills = [
-    { "name":"Java stack engineering",
-    "stars": ["star","star","star","star","star"] },
+    {"name":"Java stack engineering",
+     "stars": ["star","star","star","star","star"] },
     {"name":"CI/CD",
-    "stars": ["star","star","star","star","star"] },
+     "stars": ["star","star","star","star","star"] },
     {"name":"DevOps",
-    "stars": ["star","star","star","star_half","star_border"] },
+     "stars": ["star","star","star","star_half","star_border"] },
     {"name":"Full-stack engineering",
-    "stars": ["star","star","star","star","star_border"] },
+     "stars": ["star","star","star","star","star_border"] },
     {"name":"Software test automation",
-    "stars": ["star","star","star","star","star_border"] },
+     "stars": ["star","star","star","star","star_border"] },
     {"name":"Software architecture",
-      "stars": ["star","star","star","star","star_border"] },
+     "stars": ["star","star","star","star","star_border"] },
   ]
 
   softSkills = [
-    { "name":"Technical team leading",
-    "stars": ["star","star","star","star","star"] },
+    {"name":"Technical team leading",
+     "stars": ["star","star","star","star","star"] },
     {"name":"Agility",
-    "stars": ["star","star","star","star","star_half"] },
+     "stars": ["star","star","star","star","star_half"] },
     {"name":"Motivating/Inspiring teams",
-    "stars": ["star","star","star","star_half","star"] },
+     "stars": ["star","star","star","star_half","star"] },
     {"name":"Taking ownership",
-    "stars": ["star","star","star","star","star"] },
+     "stars": ["star","star","star","star","star"] },
     {"name":"Communication",
-    "stars": ["star","star","star","star","star"] },
-    {"name":"Communication",
+     "stars": ["star","star","star","star","star"] },
+    {"name":"Public speaking",
       "stars": ["star","star","star","star_half","star_border"] },
   ]
 
-  options = {"accordion":false}
-constructor() { }
+  constructor() { }
 
-ngOnInit() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, this.options);
-}
+  ngOnInit() {
+    var collapsibleElems = document.querySelectorAll('.collapsible' );
+    var collapsibleInstances = M.Collapsible.init(collapsibleElems, {"accordion":false});
+  }
 
 }
