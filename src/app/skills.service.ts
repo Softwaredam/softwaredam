d38ab58fbe01372fae83051fe4567a47ec5d1aec
Yosuf <yosuf.haydary@gmail.com>
2018-11-12 17:04:59 +0100

    Introduce Skills Service

---
+++ src/app/skills.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Skill } from './skill'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SkillsService {
+
+  HARD: Skill[] = [
+    {"name":"Java stack engineering",
+     "stars": ["star","star","star","star","star"] },
+    {"name":"CI/CD",
+     "stars": ["star","star","star","star","star"] },
+    {"name":"DevOps",
+     "stars": ["star","star","star","star_half","star_border"] },
+    {"name":"Full-stack engineering",
+     "stars": ["star","star","star","star","star_border"] },
+    {"name":"Software test automation",
+     "stars": ["star","star","star","star","star_border"] },
+    {"name":"Software architecture",
+     "stars": ["star","star","star","star","star_border"] },
+  ];
+
+  SOFT: Skill[] = [
+    {"name":"Technical team leading",
+     "stars": ["star","star","star","star","star"] },
+    {"name":"Agility",
+     "stars": ["star","star","star","star","star_half"] },
+    {"name":"Motivating/Inspiring teams",
+     "stars": ["star","star","star","star_half","star"] },
+    {"name":"Taking ownership",
+     "stars": ["star","star","star","star","star"] },
+    {"name":"Communication",
+     "stars": ["star","star","star","star","star"] },
+    {"name":"Public speaking",
+      "stars": ["star","star","star","star_half","star_border"] },
+  ];
+
+  constructor() { }
+
+  getSoftSkills(): Skill[] {
+    return this.SOFT;
+  }
+
+  getHardSkills(): Skill[] {
+    return this.HARD;
+  }
+}
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as M from 'materialize-css/dist/js/materialize.min.js'
+import { Skill } from '../skill';
+import { SkillsService } from '../skills.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,41 +9,19 @@ import * as M from 'materialize-css/dist/js/materialize.min.js'
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  hardSkills = [
-    {"name":"Java stack engineering",
-     "stars": ["star","star","star","star","star"] },
-    {"name":"CI/CD",
-     "stars": ["star","star","star","star","star"] },
-    {"name":"DevOps",
-     "stars": ["star","star","star","star_half","star_border"] },
-    {"name":"Full-stack engineering",
-     "stars": ["star","star","star","star","star_border"] },
-    {"name":"Software test automation",
-     "stars": ["star","star","star","star","star_border"] },
-    {"name":"Software architecture",
-     "stars": ["star","star","star","star","star_border"] },
-  ]
 
-  softSkills = [
-    {"name":"Technical team leading",
-     "stars": ["star","star","star","star","star"] },
-    {"name":"Agility",
-     "stars": ["star","star","star","star","star_half"] },
-    {"name":"Motivating/Inspiring teams",
-     "stars": ["star","star","star","star_half","star"] },
-    {"name":"Taking ownership",
-     "stars": ["star","star","star","star","star"] },
-    {"name":"Communication",
-     "stars": ["star","star","star","star","star"] },
-    {"name":"Public speaking",
-      "stars": ["star","star","star","star_half","star_border"] },
-  ]
+  hardSkills: Skill[] = []
 
-  constructor() { }
+  softSkills: Skill[] = []
+
+  constructor(private skillsService: SkillsService) { }
 
   ngOnInit() {
     var collapsibleElems = document.querySelectorAll('.collapsible' );
     var collapsibleInstances = M.Collapsible.init(collapsibleElems, {"accordion":false});
-  }
 
+    this.softSkills = this.skillsService.getSoftSkills();
+    this.hardSkills = this.skillsService.getHardSkills();
+
+  }
 }
