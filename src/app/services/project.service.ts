0102bb5f43c26270a0e9f1ab2fd63a063e0d4539
Yosuf Haydary <yosuf.haydary@rws.nl>
2021-04-30 13:42:56 +0200

    Update content

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -23,12 +23,22 @@ export class ProjectService {
         "Currently, I am helping 6 teams, and around 80+ software components to migrate to a new platform. It involves migrating from Docker Swarm to Kubernetes for hosting, and from Bitbucket+Jenkins to GitLab.",
         "Beside migration I am helping the teams use the full potential of the platform and deliver software with even more quality."
       ],
-      roles: ["Migration and Integration Architect", "Technical Lead" ],
+      roles: ["Migration and Integration Architect", "Technical Lead"],
       activities: [
-        "Requirements Engineering", "Planning and exection of platform migration from Docker swarm to Kubernetes."
+        "Requirements Engineering",
+        "Development and testing",
+        "DevOps",
+        "Planning and exection of platform migration from Docker swarm to Kubernetes",
+        "Presenting workshops and hands-on sessions"
       ],
       tools: [
-        "Kubernetes","Docker swarm",  "Harbor", "Jenkins", "GitLab", "PostgreSQL", "Java", "Java8", "Java11", "Maven", "Nginx", "Apache", "Traefik", "Helm", "Ansible", "WSO2AM", "WSO2IS", "Postfix", "..."
+        "Kubernetes", "Docker (swarm)", "Harbor", "Jenkins",
+        "Java/JDK", "Java 8", "Java 11", "Java 14",
+        "Spring", "Micronaut",
+        "Maven",
+        "GitLab", "PostgreSQL",
+        "Nginx", "Apache", "Traefik", "Helm", "Ansible",
+        "WSO2AM", "WSO2IS", "Postfix", "..."
       ],
     },
     {
@@ -48,12 +58,20 @@ export class ProjectService {
         "BvB is implemented with NoOps and DevOps concepts in mind. It is fully automated. Features are automatically tested and promoted to different environments like acceptance, pre-production and production. Everything, including configuration, secrets, certificates, keys and so on are handled securely as code.",
         "BvB resides on the Standard Platform of the Dutch government (Ministerie van Infrastructuur en Waterstaat). It offers a modern platform for developing and hosting of applications. Some of the main blocks are Kubernetes (Openshift), Docker, GitLab, Harbor, and more."
       ],
-      roles: ["Solution Architect", "Technical Lead" ],
+      roles: ["Solution Architect", "Technical Lead"],
       activities: [
-        "Agile solution shaping to support DevOps, CI/CD and Continuous Quality", "Requirements Engineering", "Backlog management", "Team management"
+        "DevOps",
+        "Agile solution shaping to support DevOps, CI/CD and Continuous Quality",
+        "Development and testing",
+        "Requirements Engineering",
+        "Backlog management",
+        "Team management"
       ],
       tools: [
-        "Microservice", "Kubernetes","Docker", "Harbor", "WSO2AM", "PostgreSQL", "Java", "Java8", "Java11", "Gradle", "REST", "SOAP", "XML", "Json", "Security", "PKI PKIoverheid", "GitLab",
+        "Microservice", "Kubernetes", "Docker", "GitLab", "Harbor", "WSO2AM",
+        "PostgreSQL", "Java", "Java 8", "Java 11",
+        "Spring", "Micronaut",
+        "Gradle", "REST", "SOAP", "XML", "Json", "Security", "PKI PKIoverheid",
       ],
     },
 
@@ -79,7 +97,7 @@ export class ProjectService {
         "DevOps", "CI/CD", "Developing, QA and taking of functionality to production, mainly in the backend", "Integration of microservices"
       ],
       tools: [
-        "Kubernetes","Docker", "Harbor", "Azure", "Microservice architecture", "Java", "Java8", "Gradle", "REST", "SOAP", "XML", "Json", "Security", "PKI PKIoverheid", "Oauth2 openid connect", "JWT", "GitLab",
+        "Kubernetes", "Docker", "Harbor", "Azure", "Microservice architecture", "Java", "Java8", "Gradle", "REST", "SOAP", "XML", "Json", "Security", "PKI PKIoverheid", "Oauth2 openid connect", "JWT", "GitLab",
       ],
     },
 
@@ -99,7 +117,7 @@ export class ProjectService {
         "Softwaredam.com is designed and implemented as responsive and mobile/desktop friendly as possible.",
         "It runs on GitHub Pages and it is licensed under MIT."
       ],
-      roles: ["Front-end engineer","PO",],
+      roles: ["Front-end engineer", "PO",],
       activities: [
         "Thinking of functionality as PO", "UX/UI Design", "Website development", "Portfolio development", "Exploring and learning Angular 7 and Materialize"
       ],
@@ -122,9 +140,13 @@ export class ProjectService {
         "WLIS is ProRail's new information system for Cargo Trains and Wagons. It is responsible for keeping the overview of the cargo train which drive or stand still on Dutch rails. This system is used by ProRail and its carrier partners.",
         "The WlIS back-end is built in Java 8 and Spring Boot. The front-end is built in ReactJS, Typescript and Material UI."
       ],
-      roles: ["Technical Lead", "Scrum-master", "Solution Architect", "CI/CD specialist" ],
+      roles: ["Technical Lead", "Scrum-master", "Solution Architect", "CI/CD specialist"],
       activities: [
-        "Customer communication", "Partnering with ProRail to choose a fit strategy", "Leading the scrum process intern/extern", "Coaching and helping the dev-team",
+        "Customer communication", 
+        "Development and testing",
+        "Partnering with ProRail to choose a fit strategy", 
+        "Leading the scrum process intern/extern", 
+        "Coaching and helping the dev-team",
       ],
       tools: [
         "Java 8", "Spring boot", "REST", "JBoss", "React", "Materialize-UI", "CI/CD", "Typescript", "Jenkins", "Maven", "EMS", "..."
@@ -148,10 +170,10 @@ export class ProjectService {
       ],
       roles: ["Medior Java Software Engineer", "Quality analist"],
       activities: [
-        "Implemting and testing user stories", "Helping with design of the UI", "Taking part in all activiteis of the scrum team"
+        "Implementation and testing of user stories", "Helping with design of the UI", "Taking part in all activiteis of the scrum team"
       ],
       tools: [
-        "Java SE/EE","JavaFX 1.3/2.0", "Maven", "Jenkins", "Junit", "WSDL", "XML", "SOAP", "SIG (Software Improvement Group)",
+        "Java SE/EE", "JavaFX 1.3/2.0", "Maven", "Jenkins", "Junit", "WSDL", "XML", "SOAP", "SIG (Software Improvement Group)",
       ],
     },
 
@@ -169,13 +191,13 @@ export class ProjectService {
         "RITC is a message driven java backend component which gathers and distributes rails components information.",
         "As Technical Lead and Solution Architect I helped the team startup the project and reuse software modules from previous ProRail software."
       ],
-      roles: [ "Technical Lead", "Solution Architect", "Senior Software Engineer" ],
+      roles: ["Technical Lead", "Solution Architect", "Senior Software Engineer"],
       activities: [
         "Customer communication",
         "Architectural decisions",
         "Changes evaluation",
         "Coaching the team",
-        "Implementing features",
+        "Development and testing",
         "Backlog maintenance",
       ],
       tools: [
@@ -198,18 +220,18 @@ export class ProjectService {
         "ISVL is a web application which is used by train dispatcher, traffic controllers and other parties in and around ProRail for communication of last minute orders. ",
         "As a technical lead I was responsible for both the technical aspects and process aspects of the development team. I evaluated the proposals and changes, analyzed and solved major performance problems, coached the team, and took part in writing features."
       ],
-      roles: [ "Technical Lead", "Solution Architect", "Senior Software Engineer" ],
+      roles: ["Technical Lead", "Solution Architect", "Senior Software Engineer"],
       activities: [
         "Customer communication",
         "Architectural decisions",
         "Solve major performance issues",
         "Changes evaluation",
         "Coaching the team",
-        "Implementing features",
+        "Development and testing",
         "Backlog maintenance"
       ],
       tools: [
-        "Java 6/8", "WebLogic", "11c" ,"12g", "Spring", "Hibernate", "Vaadin 7", "Icepush", "Jaxb", "JMS", "Sprintboot", "REST", "Team stabalization" , "Automatic regression tests", "load test", "Vaadin Testbench", "PhantomJS", "WSDL", "SOAP", "XML", "Maven", "Jenkins", "Nexus", "Sonar", "CQA"
+        "Java 6/8", "WebLogic", "11c", "12g", "Spring", "Hibernate", "Vaadin 7", "Icepush", "Jaxb", "JMS", "Sprintboot", "REST", "Team stabalization", "Automatic regression tests", "load test", "Vaadin Testbench", "PhantomJS", "WSDL", "SOAP", "XML", "Maven", "Jenkins", "Nexus", "Sonar", "CQA"
       ],
     },
 
@@ -227,9 +249,9 @@ export class ProjectService {
         "Train Position System Train is a highly available system which gathers and distributes train positions.",
         "As a senior software engineer I was part of a multidisciplinary scrum team who implemented and tested features. Later on I was also responsible for leading the dev-team. Together with the team I was responsible for a high 5-star SIG (min. 4-star) quality system."
       ],
-      roles: [ "Senior Software Engineer"  ],
+      roles: ["Senior Software Engineer"],
       activities: [
-        "Implement and test features",
+        "Development and testing",
         "Take part in a multidisciplinary scrum team",
         "Customer communication",
         "Quality analysis",
@@ -253,7 +275,7 @@ export class ProjectService {
       description: [
         "Dev@CGI is a community for developers in CGI NL. As part of the core team and later on head of the Dev@CGI community I organized workshops and external speakers sessions for the developers. I also formed an interface to the management."
       ],
-      roles: [ "Dev visionary"  ],
+      roles: ["Dev visionary"],
       activities: [
         "Communication",
         "Organize community meetings and workshops",
@@ -267,7 +289,7 @@ export class ProjectService {
 
     {
       id: 5,
-      key: "cicd",
+      key: "cgi-cicd",
       name: "CI/CD Envs",
       customer: "CGI",
       start: "06/2014",
@@ -278,7 +300,7 @@ export class ProjectService {
       description: [
         "As a CI/CD specialst I have mostly taken the responsibility to set up an integrated development and test environment and focus on new tools and concepts. During this period I also helped multiple projects to adapt new tools and successfully migrate from old environments. "
       ],
-      roles: [ "CI/CD Specialist" ],
+      roles: ["CI/CD Specialist"],
       activities: [
         "Set up and integrate new tools",
         "Coach teams adapt the new tools",
@@ -375,7 +397,7 @@ export class ProjectService {
         "TTE Robot concept is part of Graham Smith's art and concept.",
         "In my role as software developer I created a system which consisted of 4 applications. The user application runs on the user's machine and connects with the Robot driver app through the Skype API. The user can see through the robot's HD webcam, drive the robot and see on a map what the location of the robot is. The Robot appilcation runs on a Mac-mini on the robot and controls the motors through an arduino, the RFID reader and the camera. The arduino application is responsible for accepting and translating the user commands into the right steps on the motor. A back-end application written in PHP is responsible for inital handshake between the user and the robot and also makes sure the FIFO principle.",
       ],
-      roles: [ "Java Software Developer", "Junior hardware engineer" ],
+      roles: ["Java Software Developer", "Junior hardware engineer"],
       activities: [
         "Implementing requirements",
         "Implementing the front-end designed by an expert",
@@ -389,10 +411,10 @@ export class ProjectService {
 
   ];
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
 
   getProjects(): Observable<Project[]> {
-    return of(this.PROJECTS.sort((a,b) => {
+    return of(this.PROJECTS.sort((a, b) => {
       let as: string[] = a.start.split("/");
       let bs: string[] = b.start.split("/");
       return bs[1].localeCompare(as[1]) || bs[0].localeCompare(as[0]);
